bcbbcc5ce17dc3fcd562a4cfe64c77b71cb786c3
Tickey <geogdzhayantigran@gmail.com>
2026-05-01 19:45:50 +0300

    Add project files.

---
+++ Calc_2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calc_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstVal, secondVal;
+                string action; 
+                try
+                {
+
+                    Console.WriteLine("Number 1");
+                    firstVal = double.Parse(Console.ReadLine());    
+
+                    Console.WriteLine("Number 2");
+                    secondVal = double.Parse(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Incorrect Value");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Enter the action");
+                action = Console.ReadLine();
+
+                if (action == "+")
+                {
+                    Console.WriteLine(firstVal + secondVal);
+                }
+                else if (action == "-")
+                {
+                    Console.WriteLine(firstVal - secondVal);
+                }
+                else if (action == "*")
+                {
+                    Console.WriteLine(firstVal * secondVal);
+                }
+                else if (action == "/")
+                {
+                    if (secondVal == 0)
+                    {
+                        Console.WriteLine("You can't divide on 0. Lol 67676767");
+                    }
+                    else
+                    {
+                        Console.WriteLine(firstVal / secondVal);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MyProject/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint oddNum = 0;
+            uint evenNum = 0;
+
+            int oddNumSum = 0;
+            int evenNumSum = 0;
+
+            Console.WriteLine("Первое число диапозона");
+            int curNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Второе число диапозона");
+            int maxNum = int.Parse(Console.ReadLine());
+
+            while (curNum <= maxNum)
+            {
+
+                if (curNum % 2 == 0)
+                {
+                    evenNum++;
+                    evenNumSum += curNum;
+                }
+                else if (curNum % 2 != 0)
+                {
+                    oddNum++;
+                    oddNumSum = curNum;
+                };
+
+                curNum++;
+            };
+                    Console.WriteLine("Odd nums count: " + oddNum);
+                    Console.WriteLine("Even nums count: " + evenNum);
+                    Console.WriteLine("Sum of odd nums: " + oddNumSum);
+                    Console.WriteLine("Sum of even nums: " + evenNumSum);
+                    Console.ReadLine();
+
+        }
+    }
+}
